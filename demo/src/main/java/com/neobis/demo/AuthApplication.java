35678d486ef11f5159845f835ccc2c34dc070813
dell <m>
2023-12-04 17:12:58 +0600

    Deploy to railway app

--- demo/src/main/java/com/neobis/demo/AuthApplication.java
+++ demo/src/main/java/com/neobis/demo/AuthApplication.java
@@ -1,11 +1,15 @@
 package com.neobis.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.neobis.demo")
+@OpenAPIDefinition(servers = {
+		@Server(url = "/", description = "Default Server URL")})
 public class AuthApplication {
 
 	public static void main(String[] args) {
